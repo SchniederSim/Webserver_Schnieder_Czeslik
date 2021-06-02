1f765f5358667b55e8dac4722eaf597f71788f73
Jonas C <jonas.czeslik@fluxatron.com>
2021-06-02 07:07:42 +0200

    removed index.css and adapted with php

---
+++ html/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webshop</title>
+    <script src="scripts/jquery-3.6.0.min.js"></script>    
+    <script type="module" src="scripts/navbar.js"></script>
+    <!-- <script type="module" src="./scripts/product.js"></script> -->
+
+    <!-- <script type="module" src="./product-script.js"></script> -->
+    <!-- <script src="jquery-test.js"></script> -->
+
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="global-styles.css">
+</head>
+<body>
+    <div class="navbar-page-container">
+        <custom-navbar></custom-navbar>
+        <div class="page">
+            <div class="page-container">
+                <h1 class="header">Main Page</h1>
+                <p>Hallo</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
