7f0ecd02cf95416ab1f9e847cc6fbaf9a6135d39
Simon Schnieder <simon.schnieder@stud.hs-ruhrwest.de>
2021-06-06 22:09:14 +0200

    Database connection with first functions to retrieve data.

--- index.js
+++ index.js
@@ -3,7 +3,57 @@ const app = express();
 app.use(express.static(__dirname + '/html'));
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+var mysql = require('mysql');
+var dbConnection = mysql.createConnection({
+  host: 'remotemysql.com',
+  port: '3306',
+  user: 'YEfPnCDIbs',
+  password: 'U1j9QBQ531',
+  database: 'YEfPnCDIbs'
+});
+var hasDBConntection = false;
 
+dbConnection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  hasDBConntection = true;
+});
+
+function getAllUsers() {
+  var result;
+  dbConnection.query('SELECT * FROM USERS', function (err, rows, fields) {
+    if (err) throw err;
+    console.log(rows[0].Username);
+    result = rows;
+  });
+  
+  return result;
+}
+function getAllProducts(){
+  var result;
+  dbConnection.query(
+    'SELECT PRODUCTS.Name, PRODUCTS.InStorage, PRODUCTS.Price, PRODUCERS.ProducerName FROM PRODUCTS, PRODUCERS WHERE PRODUCTS.ProducerId = PRODUCERS.ProducerId ORDER BY Name ', function (err, rows, fields) {
+    if (err) throw err;
+    console.log(rows[0].ProducerName);
+    result = rows;
+  });
+  
+  return result;
+}
+
+function getAllPurchasesOfUser(userId){
+  var result;
+  dbConnection.query('SELECT PRODUCTS.Name, PRODUCERS.ProducerName, USERS.Username, PURCHASES.Amount, PURCHASES.totalPrice, PURCHASES.Timestamp FROM PURCHASES, PRODUCTS, PRODUCERS, USERS WHERE PURCHASES.UserId = '+userId+' AND PURCHASES.ProductId = PRODUCTS.ProductId AND PRODUCTS.ProducerId = PRODUCERS.ProducerId AND PURCHASES.UserId = USERS.UserId' , function (err, rows, fields) {
+    if (err) throw err;
+    console.log(rows[1].ProducerName);
+    result = rows;
+  });
+
+  return result;
+}
+getAllUsers();
+getAllProducts();
+getAllPurchasesOfUser(1);
 
 
 app.get('/', (req, res) => {
@@ -12,7 +62,6 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('user connected');
-  var timeouts = [];
 
   socket.on('message-send', (message) => {
     console.log('message-send: ' + message);
